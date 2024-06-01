20028f83751f572a64124ef726fd4894f4dbae39
Wilson Capacho <wiston.98@hotmail.com>
2024-06-01 16:54:42 -0500

    creacion de modal

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, reactive} from 'vue';
 import Presupuesto from './components/Presupuesto.vue';
+import ControlPresupuesto from './components/ControlPresupuesto.vue';
+import Modal from './components/Modal.vue';
+import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
+
+const modal = reactive({
+  mostrar:false,
+  animar: false
+})
 
 const presupuesto = ref(0)
+const disponible = ref(0)
 
 const definirPresupuesto = (cantidad) => {
   presupuesto.value = cantidad
+  disponible.value = cantidad //
+}
+
+const mostrarModal = () =>{
+  modal.mostrar = true
+  modal.animar = true
 }
 </script>
 
@@ -20,12 +35,34 @@ const definirPresupuesto = (cantidad) => {
           v-if="presupuesto === 0"
           @definir-presupuesto="definirPresupuesto"
         />
-        <p v-else>Presupuesto Valido</p>
+        <ControlPresupuesto 
+          v-else
+          :presupuesto="presupuesto"
+          :disponible="disponible"
+        />
       </div>
     </header>
 
+   
+
     <Filtro />
   </div>
+
+
+  <main v-if="presupuesto > 0 ">
+      <div class="crear-gasto">
+        <img
+          :src="iconoNuevoGasto"
+          alt="icono nuevo gasto"
+          @click="mostrarModal"
+        >
+      </div>
+
+      <Modal 
+        v-if="modal.mostrar"
+      />
+    </main>
+
 </template>
 
 <style>
@@ -93,6 +130,21 @@ const definirPresupuesto = (cantidad) => {
   background-color: var(--blanco);
   border-radius: 1.2rem;
   padding: 5rem;
+ }
+
+ .crear-gasto{
+  position: fixed;
+  bottom: 5rem;
+  right: 5rem;
+ }
+
+ .crear-gasto img{
+    width: 5rem;
+    cursor: pointer;
+ }
+
+ .crear-gasto img:hover{
+    
  }
 
 </style>
---
+++ src/components/ControlPresupuesto.vue
@@ -0,0 +1,118 @@
+<script setup>
+    import imagen from "../assets/img/grafico.jpg";
+    import {formatearCantidad} from '../helpers'
+
+    const props = defineProps({
+        presupuesto:{
+            type: Number, 
+            required: true
+        },
+        disponible:{
+            type: Number, 
+            required: true
+        }
+    })
+</script>
+
+
+<template>
+    <div class="dos-columnas">
+        <div class="contenedor-grafico">
+            <img 
+                :src="imagen" alt="prueba"
+            >
+        </div>
+
+        <div class="contenedor-presupuesto">
+            <button class="reset-app">
+                Resetear App
+            </button>
+
+            <p>
+                <span>
+                    Presupuesto:
+                </span>
+                 {{ formatearCantidad(presupuesto) }}
+            </p>
+
+            <p>
+                <span>
+                    Disponible:
+                </span>
+                 {{ formatearCantidad(disponible) }}
+            </p>
+
+            <p>
+                <span>
+                    Gastado:
+                </span>
+                $0
+            </p>
+        </div>
+
+    </div>
+</template>
+
+
+<style scoped>
+    .dos-columnas{
+       display: flex;
+       flex-direction: column; 
+    }
+
+    .dos-columnas > :first-child{
+        margin-bottom: 4rem;
+    }
+
+    @media (min-width: 768px){
+        .dos-columnas{
+                flex-direction: row; 
+                gap: 4rem;
+                align-items: center;
+            }
+
+        .dos-columnas > :first-child{
+             margin-bottom: 0;
+        }
+    }
+
+    .reset-app{
+        background-color: #DB2777;
+        border: none;
+        padding: 1rem;
+        width: 100%;
+        color: var(--blanco);
+        border-radius: 10px;
+        font-weight: 900;
+        text-transform: uppercase;
+        transition-property: background-color;
+        transition-duration: 300ms;
+    }
+
+    .reset-app:hover{
+        background-color: #7b0239;
+        cursor: pointer;
+        color: var(--blanco);
+    }
+
+    .contenedor-presupuesto{
+        width: 100%;
+    }
+
+    .contenedor-presupuesto p{
+        font-size: 2.4rem;
+        text-align: center;
+        color:rgb(69, 69, 69);
+    }
+
+    @media (min-width: 768px){
+        .contenedor-presupuesto p{
+            text-align: left;
+        }
+    }
+
+    .contenedor-presupuesto span{
+        font-weight: 900;
+        color: var(--azul);
+    }
+</style>
