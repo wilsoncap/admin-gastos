0b8b93bac731169b3704d63a2054700608faab0a
Wilson Capacho <wiston.98@hotmail.com>
2024-06-02 22:49:13 -0500

    primeros pasos mostrando los gastos

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import {ref, reactive} from 'vue';
 import Presupuesto from './components/Presupuesto.vue';
 import ControlPresupuesto from './components/ControlPresupuesto.vue';
+import Gasto from './components/Gasto.vue';
 import Modal from './components/Modal.vue';
 import {generarId} from './helpers';
 import iconoNuevoGasto from './assets/img/nuevo-gasto.svg'
@@ -48,7 +49,16 @@ const guardarGasto = () => {
       ...gasto,
       id: generarId()
     })
-    console.log('gasto: ', gasto);
+    
+    ocultarModal()
+
+    Object.assign(gasto, {
+      nombre: '',
+      cantidad: '',
+      categoria: '',
+      id: null,
+      fecha: Date.now()
+    })
 }
 </script>
 
@@ -78,6 +88,17 @@ const guardarGasto = () => {
 
 
   <main v-if="presupuesto > 0 ">
+
+      <div class="listado-gastos contenedor">
+        <h2>{{ gastos.length > 0 ? 'Gastos' : 'No hay gastos' }}</h2>
+
+        <Gasto 
+          v-for="gasto in gastos"
+          :key="gasto.id"
+          :gasto="gasto"
+        />
+      </div>
+
       <div class="crear-gasto">
         <img
           :src="iconoNuevoGasto"
@@ -177,8 +198,13 @@ const guardarGasto = () => {
     cursor: pointer;
  }
 
- .crear-gasto img:hover{
-    
+ .listado-gastos{
+    margin-top: 10rem;
+ }
+
+ .listado-gastos h2{
+    font-weight: 700;
+    color: var(--gris-oculo);
  }
 
 </style>
---
+++ src/components/Gasto.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+
+<template>
+    <div>
+        <p>Gasto...</p>
+    </div>
+</template>
+
+
+<style scoped>
+</style>
